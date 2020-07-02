d95c8802cf1b17d5451936a67e8158530ebeef08
Lorenzo Monacelli <mesonepigreco@gmail.com>
2020-07-02 15:50:14 +0200

    Now also the analysis on the final result works

--- Modules/track_directory.py
+++ Modules/track_directory.py
@@ -5,6 +5,7 @@ import sys, os
 import hashlib
 import json
 
+import paramiko
 import time, datetime
 
 
@@ -16,6 +17,8 @@ FULL_PATH = "full_path"
 MOD_TIME = "last_time"
 TYPE = "file_type"
 DIR_CONTENT = "the_content"
+
+MONTHS = {"Jan" : 1, "Feb":2, "Mar" : 3, "Apr":4, "May":5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep":9, "Oct":10, "Nov":11, "Dec":12}
 
 class Directory:
 
@@ -109,6 +112,80 @@ class Directory:
         
         return data_in_dir
 
+
+class RemoteDirectory(Directory):
+
+    def __init__(self):
+        # Initialize the directory
+        Directory.__init__(self)
+
+        # Initialize specific remote controls
+        self.hostname = ""
+        self.username = ""
+        self.pwd = ""
+        self.port = 22
+        self.busybox = True
+
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.load_system_host_keys()
+
+    def configure_connection(self, **kwargs):
+        """
+        Configure the SSH connection.
+        Beside hostname, password and port, you can specify extra keyword arguments to be passed to the SSHClient
+        """
+        
+        self.ssh_client.connect(self.hostname, username= self.username, port = self.port, password = self.pwd, **kwargs)
+
+    def track_files(self, absolute_path):
+        """
+        Tracks the files.
+        """
+
+        previous_dir = os.path.join(*os.path.split(absolute_path)[:-1])
+        dir_name = os.path.split(absolute_path)[-1]
+
+        # Produce the original matrix
+        print("Analyzing:", previous_dir)
+        total_info = analyze_directory_ssh(self.ssh_client, previous_dir, busybox= self.busybox)
+
+        print(total_info)
+
+        index = -1
+        self.total_info = {}
+        for i, value in enumerate(total_info):
+            if value[FULL_PATH] == absolute_path:
+                self.total_info = value
+                break 
+
+        if len(self.total_info) == 0:
+            raise ValueError("Error, something went wrong!")
+
+        self.total_info = self._track_files(self.total_info)
+
+    def _track_files(self, current_info):
+        """
+        Working method
+        """
+
+        if current_info[TYPE] == "file":
+            return current_info
+
+        # Analyze the files
+        new_info_list = analyze_directory_ssh(self.ssh_client, current_info[FULL_PATH], busybox= self.busybox)
+
+        dir_content = []
+        for i in range(len(new_info_list)):
+            dir_content.append(self._track_files(new_info_list[i]))
+        
+        current_info[DIR_CONTENT] = dir_content 
+
+        return current_info
+
+
+
+
+
 def analyze_file(path, get_md5 = True):
     """
     Provide the full dictionary that identifies the given file
@@ -149,7 +226,7 @@ def analyze_file(path, get_md5 = True):
     return infofile
 
 
-def analyze_directory_ssh(ssh_connect, path, pwd = None):
+def analyze_directory_ssh(ssh_connect, path, pwd = None, busybox = False):
     """
     Get the info on the files through an ssh connection inside the directory.
 
@@ -167,15 +244,20 @@ def analyze_directory_ssh(ssh_connect, path, pwd = None):
             The dictionary with the analysis of the directory.
     """
 
-    stdin, stdout, stderr = ssh_connect.exec_command(r"ls -l --time-style=+%s  " + path)
+    if not busybox:
+        cmd = r"ls -l --time-style=+%s  " + path
+    else:
+        cmd = r"ls -l -e  " + path
+
+    stdin, stdout, stderr = ssh_connect.exec_command(cmd)
     
     # Write the password
     if not pwd is None:
         stdin.write("{}\n".format(pwd))
 
-    return analyze_with_ls(stdout, stderr, total_path = path)
+    return analyze_with_ls(stdout.readlines(), stderr.readlines(), total_path = path, busybox= busybox)
 
-def analyze_with_ls(stdout, stderr, total_path):
+def analyze_with_ls(stdout, stderr, total_path, busybox = False):
     """
     Analyze the output of 
     ls -l --time-style=+%s
@@ -189,6 +271,10 @@ def analyze_with_ls(stdout, stderr, total_path):
             The error that comes from a ls -lt command
         total_path : string
             The path of the directory that we are analyzing
+        busybox : bool
+            If true use the ls from busybox. In this case, it requires the ls -l -e options.
+            This is usefull since some NAS have this limited version of ls that is not able
+            to exploit the time-style option.
     
     Returns
     -------
@@ -201,16 +287,16 @@ def analyze_with_ls(stdout, stderr, total_path):
 
 
     # Analyze the output
-    lines = stdout.split("\n")
-    
     all_components = []
 
-    for line in lines:
+    for line in stdout:
+        line = line.strip()
         data = line.split()
 
         # Keep only good data
-        if len(data != 7):
-            continue 
+        if not busybox:
+            if len(data != 7):
+                continue 
 
         filedata = {}
 
@@ -221,17 +307,35 @@ def analyze_with_ls(stdout, stderr, total_path):
             filedata[TYPE] = "file"
         
         try:
+            # If busybox reshape the data
+            if busybox:
+                data[10] = " ".join(data[10:])
+                data = data[:11]
+
             # Get the date
-            filedata[LAST_EDIT] = int(data+[-2])
+            if not busybox:
+                filedata[LAST_EDIT] = int(data[-2])
+            else:
+                # Read the data with busybox
+                year = int(data[-2])
+                month = MONTHS[data[-5]]
+                day = int(data[-4])
+                hour = int(data[-3].split(":")[0])
+                minute = int(data[-3].split(":")[1])
+                second = int(data[-3].split(":")[2])
+
+                date = datetime.datetime(year, month, day, hour, minute, second)
+                filedata[LAST_EDIT] = int(date.timestamp())
 
             # Get the full path
-            filedata[FULL_PATH] = total_path
+            filedata[FULL_PATH] = os.path.join(total_path, data[-1])
 
             filedata[NAME] = data[-1]
 
             all_components.append(filedata)
         except:
             print("Unrecognized line:", line)
-            raise ValueError("Error while parsing ls output.")
+            raise
 
     return all_components
+
--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -14,5 +14,19 @@ def test_analisys():
     # Save the json
     Dir.save_config("this_project.json")
 
+def test_remote_analysis():
+    total_path = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(total_path)
+
+    RemoteDir = td.RemoteDirectory()
+    RemoteDir.hostname = "192.168.1.102"
+    RemoteDir.username = "sshd"
+    RemoteDir.configure_connection()
+
+    RemoteDir.track_files("/mnt/HD/HD_a2/Public/Lorenzo/Simulations/COVID/COVID/quarantine")
+
+    RemoteDir.save_config("remote_dir.json")
+
 if __name__ == "__main__":
     test_analisys()
+    test_remote_analysis()
